831469a51c04cff90ef35fb81e29d3d614a6baa9
Andry Smirnov <Andry.Smirnov@gmail.com>
2023-01-12 22:27:34 +0300

    Rename

--- install/Шаг 2. Общие программы/base-apps.sh
+++ install/Шаг 2. Общие программы/base-apps.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+
+# Update and upgrade
 sudo apt-get update -y && sudo apt-get upgrade -y
+
+# 1
 sudo apt install wget gthumb pdfshuffler gtkhash xfburn -y
+
+# Disk utility
 sudo apt install gnome-disk-utility -y
+
+# Neofetch
 sudo apt install neofetch -y
+
+# Snap
 sudo apt install snapd -y
