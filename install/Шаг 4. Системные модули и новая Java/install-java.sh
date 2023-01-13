d398594d5e4c749f3694f16004cb268688afade0
Andry Smirnov <Andry.Smirnov@gmail.com>
2023-01-13 18:33:13 +0300

    Rename

--- install/Шаг 4. Системные модули и новая Java/install-java.sh
+++ install/Шаг 4. Системные модули и новая Java/install-java.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
-# Измените тут свой логин и аккаунт для Git
+# set login and e-mail for Git
 sudo apt-get install git -y
-git config --global user.name "proffix4"
-git config --global user.email "proffix4@gmail.com"
+git config --global user.name "login"
+git config --global user.email "e-mail"
 
 sudo apt install build-essential gdb openjdk-8-jdk default-jdk meson nasm -y
 
-sudo apt install curl -y
+#sudo apt install curl -y
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# java 11.0.12
 sdk install java 11.0.12-open
 sdk default java 11.0.12-open
+
+# java 17.0.2
 #sdk install java 17.0.2-open
 #sdk default java 17.0.2-open
 
 sudo apt install qemu-kvm android-tools-adb -y
-#Замените tsn на свой логин в системе
-sudo adduser tsn kvm
-sudo chown tsn /dev/kvm
+
+# set user name
+sudo adduser user-name kvm
+sudo chown user-name /dev/kvm
 sudo chmod o+x /dev/kvm
 sudo chmod 777 -R /dev/kvm
 sudo usermod -aG plugdev $LOGNAME
